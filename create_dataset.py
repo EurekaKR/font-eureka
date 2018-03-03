e4d8a7dfe7871e4368ad2c7ac79975c2c1f279f9
MinGyo Jung <jmg7173@gmail.com>
2018-03-03 18:37:10 +0900

    Convert python 2.7.x code to python 3.6.x

--- create_dataset.py
+++ create_dataset.py
@@ -1,16 +1,19 @@
-import h5py
-import PIL, PIL.ImageFont, PIL.Image, PIL.ImageDraw, PIL.ImageChops, PIL.ImageOps
 import os
-import random
-import string
-import numpy
 import sys
+
+import PIL
+import PIL.Image
+import PIL.ImageChops
+import PIL.ImageDraw
+import PIL.ImageFont
+import PIL.ImageOps
+import h5py
+import numpy
 
 w, h = 64, 64
 w0, h0 = 256, 256
 
-chars = string.uppercase + string.lowercase + string.digits
-
+chars = [''.join("uni" + str(hex(hexa)[2:].upper())) for hexa in range(0xAC00, 0xD7A4)]
 blank = PIL.Image.new('L', (w0*5, h0*3), 255)
 
 def read_font(fn):
@@ -23,7 +26,10 @@ def read_font(fn):
     imgs = []
 
     for char in chars:
-        print '...', char
+        print(f"...{char}")
+        """
+        Font full name: {char}_{fn}
+        """
         # Draw character
         img = PIL.Image.new("L", (w0*5, h0*3), 255)
         draw = PIL.ImageDraw.Draw(img)
@@ -37,7 +43,7 @@ def read_font(fn):
         max_width = max(max_width, hx - lx)
         imgs.append((lx, hx, img))
 
-    print 'crop dims:', max_hy - min_ly, max_width
+    print(f"crop dims: {max_hy - min_ly} {max_width}")
     scale_factor = min(1.0 * h / (max_hy - min_ly), 1.0 * w / max_width)
     data = []
 
@@ -53,7 +59,7 @@ def read_font(fn):
         img_sq = PIL.Image.new('L', (w, h), 255)
         offset_x = (w - new_width)/2
         offset_y = (h - new_height)/2
-        print offset_x, offset_y
+        print(offset_x, offset_y)
         img_sq.paste(img, (int(offset_x), int(offset_y)))
 
         # Convert to numpy array
@@ -76,14 +82,14 @@ dset = f.create_dataset('fonts', (1, len(chars), h, w), chunks=(1, len(chars), h
 
 i = 0
 for fn in get_ttfs(d=sys.argv[1]):
-    print fn
+    print(fn)
     try:
         data = read_font(fn)
     except: # IOError:
-        print 'was not able to read', fn
+        print('was not able to read', fn)
         continue
 
-    print data.shape
+    print(data.shape)
     dset.resize((i+1, len(chars), h, w))
     dset[i] = data
     i += 1
