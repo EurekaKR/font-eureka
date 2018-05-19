7c79f9e87c4697517a3b183e5344078ec1bfabe9
JimJeon <jimjeon.me@gmail.com>
2018-05-19 09:34:17 +0000

    Add backup api for naver storage using boto3
    Usage:
    ```
    from bak.backup_to_storage import BackUp
    s3 = BackUp(ACCESSKEY, SECRETKEY)
    ```
    
    Methods:
        - upload
        - download
        - get_list

--- bak/backup_to_storage.py
+++ bak/backup_to_storage.py
@@ -1,24 +1,43 @@
-import os
+import sys
+from glob import glob
 
-import glob
-import json
-
-import requests
+from boto3 import client
+from botocore.exceptions import ClientError
 
 
-# For every file in fonts folder
-for png in glob.glob('../fonts/*.png'):
-    file = open(png, 'rb')
-    HEADERS = {
-        "Content-Type": "image/png",
-        "Content-Length": str(os.path.getsize(png)),
-        "oauth_consumer_key": "Ysxxa5HtKx18l4LkMfRY",
-        "oauth_signature_method": "HMAC-SHA1",
-        "oauth_version": "1.0",
-    }
+class BackUp(object):
+    bucket_name = 'bak'
+    service_name = 's3'
+    endpoint_url = 'http://kr.objectstorage.ncloud.com'
+    region_name = 'kr-standard'
 
-    filename = png.split('/')[2]
-    payload = dict(body=file.read())
-    response = requests.put(url="http://restapi.fs.ncloud.com/datasets/fonts/"+filename,
-            headers=HEADERS, data=payload)
-    print(response)
+    def __init__(self, access_key, secret_key):
+        self.__access_key = access_key
+        self.__secret_key = secret_key
+
+        self.s3 = client(
+            self.service_name,
+            endpoint_url=self.endpoint_url,
+            aws_access_key_id=self.__access_key,
+            aws_secret_access_key=self.__secret_key
+        )
+
+
+    def upload(self, src_path: str, dest_path: str) -> None:
+        '''Upload files in src_path to dest_path'''
+        self.s3.upload_file(src_path, self.bucket_name, dest_path)
+
+
+    def download(self, src_path: str, dest_path: str) -> None:
+        '''Download files in src_path to dest_path'''
+        self.s3.download_file(self.bucket_name, src_path, dest_path)
+
+
+    def get_list(self):
+        '''Print all object list in s3 bucket'''
+        response = self.s3.list_objects(Bucket=self.bucket_name)
+
+        print('list all in the bucket')
+
+        for obj in response['Contents']:
+            print('{0}\t{1}'.format(obj['Key'], obj['Size']))
