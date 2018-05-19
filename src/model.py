23cc4bf88801c52129d6145be773325b13b205d4
dbdydgur2244 <dbdydgur2244@gmail.com>
2018-05-20 02:12:54 +0900

    Make api for training and testing model

--- src/model.py
+++ src/model.py
@@ -1,43 +1,23 @@
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from sklearn.model_selection import train_test_split
-import h5py
-import numpy as np
-import os
+from pathlib import Path
+import json
 
-
-RESHAPER = lambda a: a.reshape((a.shape[0], a.shape[1], a.shape[2], 1))  # TODO: PULL THIS OUT TO create_dataset.py
-
- # DEPENDENCY: create_dataset.py
- def get_data(name='fonts.hdf5'):
-    f = h5py.File(name, 'r')
-    data = np.array(f['fonts'])
-    n, k, w, h = data.shape 
-    x_input = data.reshape(-1, w, h)
-    y_input = np.zeros((n * k, n))
-    for i in range(n):
-        for j in range(k):
-            y_input[(i * k) + j][i] = 1
-    return x_input, y_input, n
-
-
-# TODO: create two dataset
-# dataset/train, dataset/test
-def load_data(x_input, y_input):
-    x_train, x_test, y_train, y_test = train_test_split(
-        x_input, y_input, test_size=0.33, random_state=42)
-    return x_train, y_train, x_test, y_test
+p = Path('.')
+model_dir = p / 'models'
 
 class Model(object):
-
-    def __init__(self, n=None):
-        self.num_classes = n
+    def __init__(self):
+        self.model = None
         # self.dataset_manifest = None
 
-    def train(self, x_train, y_train, x_test, y_test, batch_size, learning_late=0.1, epochs=1):
+    def build(self, shape, batch_size, learning_late=0.1, epochs=1):
         model = Sequential()
+
+        #Conv2D need 3D shape
         model.add(Conv2D(32, kernel_size=(3, 3),
-            activation='relu', input_shape=(x_train.shape[1], x_train.shape[2], 1)))
+                  activation='relu',
+                  input_shape=(shape[1], shape[2], 1)))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dense(1024))
 
@@ -59,36 +39,43 @@ class Model(object):
         model.compile(loss='categorical_crossentropy',
                       optimizer='adam',
                       metrics=['accuracy'])
-
-        x_train = RESHAPER(x_train)
        
-        print("x_train shape", x_train.shape, "y_train shape", y_train.shape)
-        model.fit(x_train, y_train,
-                batch_size=batch_size,
-                epochs=epochs,
-                verbose=1)
         self.model = model
-        return model
 
-    def model_test(self, x_test, y_test):
-        x_test = RESHAPER(x_test)
-        result = self.model.evaluate(x_test, y_test, verbose=1)
+    def train(self, x_train, y_train):
+        if self.model is None:
+            print("please build or load model")
+            raise ValueError
+        self.model.fit(x_train, y_train,
+                       batch_size=batch_size, epochs=epochs,nverbose=1)
+        
+    def load_model(self, model_path):
+        # TODO: how to check error in load_model
+        self.model = load_model(model_path)
+
+    def test(self, x_test, y_test):
+        model.evaluate(x_test, y_test, verbose=1)
         print(result)
         return result
 
     def save_model(self, model_name):
-        print('saving model...')
-        if not os.exists('models'):  # TODO: pathlib.
-            os.mkdir('models')
-        self.model.save('models/%s.h5' % (model_name,))
-        with open('models/%s.json' % (model_name,), 'w') as mmp:
-            import json
+        if not model_dir.exists():
+            model_dir.mkdir()
+        
+        model_path = model_dir / model_name
+        if model_path.exists():
+            print("alread exist filename")
+            raise
+
+        self.model.save(f"{model_path}.h5")
+        with open(f"{model_path}.json", 'w') as mmp:
             json.dump({
                 '_type': 'model_manifest',
                 '_version': 1,
                 'name': model_name,
                 #'using_dataset_name': self.dataset_manifest,
             }, mmp)
+        print(f"save to {model_path}.h5")
 
 if __name__ == '__main__':
     x_input, y_input, n = get_data('fonts.hdf5')
@@ -99,3 +86,18 @@ if __name__ == '__main__':
     for lr in [0.1, 0.01, 0.001]:
         model.train(x_train, y_train, x_test, y_test, batch_size=100, learning_late=lr)
         model.model_test(x_test, y_test)
+
+    '''
+    x_input, y_input = get_data('fonts.hdf5')
+    CNN_model = Model()
+    # if want to build new model then    
+    CNN_model.build(x_input.shape, batch_size, learning_late, epochs)
+    CNN_model.train(x_input, y_input)
+    # if load exist model then
+    CNN_model.load_model(model_path)
+
+    CNN_model.test(x_test, y_test)
+
+    # if want to save model
+    CNN_model.save_model(model_name) # save to models/model_name.h5
+    '''
