fc152b44f05369103c7e0907374a26571536a86e
dbdydgur2244 <dbdydgur2244@gmail.com>
2018-05-19 17:29:23 +0900

    Testing

--- src/model.py
+++ src/model.py
@@ -1,54 +1,45 @@
-import keras
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from sklearn.model_selection import train_test_split
 import h5py
 import numpy as np
+import os
 
 
-RESHAPER = lambda a: a.reshape((a.shape[0], a.shape[1], 
-                                a.shape[2], 1))
+RESHAPER = lambda a: a.reshape((a.shape[0], a.shape[1], a.shape[2], 1))  # TODO: PULL THIS OUT TO create_dataset.py
 
-def get_data():
-    f = h5py.File('fonts.hdf5', 'r')
+ # DEPENDENCY: create_dataset.py
+ def get_data(name='fonts.hdf5'):
+    f = h5py.File(name, 'r')
     data = np.array(f['fonts'])
-    n, k, w, h = data.shape
+    n, k, w, h = data.shape 
     x_input = data.reshape(-1, w, h)
-    #x_input = x_input.reshape(n * k, w, h, 1)
     y_input = np.zeros((n * k, n))
-    print(x_input.shape)
     for i in range(n):
         for j in range(k):
             y_input[(i * k) + j][i] = 1
-    return x_input, y_input, n, k, w * h
+    return x_input, y_input, n
 
 
-
+# TODO: create two dataset
+# dataset/train, dataset/test
 def load_data(x_input, y_input):
     x_train, x_test, y_train, y_test = train_test_split(
         x_input, y_input, test_size=0.33, random_state=42)
-    print('x_train', x_train.shape)
-    print('x_test', x_test.shape)
-    print('y_train', y_train.shape)
-    print('y_test', y_test.shape)
     return x_train, y_train, x_test, y_test
 
-
 class Model(object):
-    def __init__(self, n=None, k=11173, wh=75 * 75, d=40, D=1024):
-        self.n, self.k, self.d, self.D = n, k, d, D
-        self.model = None
+
+    def __init__(self, n=None):
         self.num_classes = n
+        # self.dataset_manifest = None
 
     def train(self, x_train, y_train, x_test, y_test, batch_size, learning_late=0.1, epochs=1):
         model = Sequential()
         model.add(Conv2D(32, kernel_size=(3, 3),
             activation='relu', input_shape=(x_train.shape[1], x_train.shape[2], 1)))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dense(self.D))
+        model.add(Dense(1024))
 
         model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -85,7 +76,26 @@ class Model(object):
         print(result)
         return result
 
-    def save_model(self):
+    def save_model(self, model_name):
         print('saving model...')
-        self.model.save('font_eureka_model.h5')
+        if not os.exists('models'):  # TODO: pathlib.
+            os.mkdir('models')
+        self.model.save('models/%s.h5' % (model_name,))
+        with open('models/%s.json' % (model_name,), 'w') as mmp:
+            import json
+            json.dump({
+                '_type': 'model_manifest',
+                '_version': 1,
+                'name': model_name,
+                #'using_dataset_name': self.dataset_manifest,
+            }, mmp)
 
+if __name__ == '__main__':
+    x_input, y_input, n = get_data('fonts.hdf5')
+    model = Model(n)
+
+    x_train, y_train, x_test, y_test = load_data(x_input, y_input)
+
+    for lr in [0.1, 0.01, 0.001]:
+        model.train(x_train, y_train, x_test, y_test, batch_size=100, learning_late=lr)
+        model.model_test(x_test, y_test)
